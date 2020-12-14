2db26fa3988d18f401b26f7272eefe5026486874
Lucas Simeon <lu.simeon@gmail.com>
2020-12-14 17:51:56 +0100

    Update tests to work with PHPUnit

--- tests/Builder/PostTypeBuilderTest.php
+++ tests/Builder/PostTypeBuilderTest.php
@@ -2,44 +2,42 @@
 
 use Studiometa\WPToolkit\Builders\PostTypeBuilder;
 
-it(
-	'should register a custom post type',
-	function() {
-		( new PostTypeBuilder( 'product' ) )
-		->set_labels( 'Product', 'Products' )
-		->register();
+/**
+ * PostTypeBuilder test case.
+ */
+class PostTypeBuilderTest extends WP_UnitTestCase {
+	/**
+	 * Store a PostTypeBuilder instance.
+	 */
+	public function setUp() {
+		parent::setUp();
 
-		expect( $this->type )->toBe( 'product' );
-		expect( $this->config )->toEqual(
-			array(
-				'public'           => true,
-				'show_in_rest'     => false,
-				'menu_position'    => null,
-				'has_archive'      => false,
-				'supports'         => array( 'title', 'editor', 'thumbnail' ),
-				'can_export'       => true,
-				'delete_with_user' => null,
-				'labels'           => array(
-					'name'                  => 'Products',
-					'singular_name'         => 'Product',
-					'add_new'               => 'Add New Product',
-					'add_new_item'          => 'Add New Product',
-					'edit_item'             => 'Edit Product',
-					'new_item'              => 'New Product',
-					'all_items'             => 'All Products',
-					'view_item'             => 'View Product',
-					'search_items'          => 'Search Products',
-					'not_found'             => 'No Products',
-					'not_found_in_trash'    => 'No Products found in Trash',
-					'parent_item_colon'     => '',
-					'menu_name'             => 'Products',
-					'insert_into_item'      => 'Insert into product',
-					'uploaded_to_this_item' => 'Uploaded to this product',
-					'items_list'            => 'Products list',
-					'items_list_navigation' => 'Products list navigation',
-					'filter_items_list'     => 'Filter products list',
-				),
-			)
-		);
+		$this->post_type_builder = new PostTypeBuilder( 'product' );
 	}
-);
+
+	/**
+	 * Test register function.
+	 *
+	 * @return bool
+	 */
+	public function test_post_type_builder_register() {
+		$this->post_type_builder->register();
+
+		$this->assertTrue( post_type_exists( 'product' ) );
+	}
+
+	/**
+	 * Test set_labels function.
+	 *
+	 * @return bool
+	 */
+	public function test_post_type_builder_set_labels() {
+		$this->post_type_builder->set_labels( 'Product', 'Products' );
+
+		$config = $this->post_type_builder->get_config();
+
+		$this->assertTrue( isset( $config['labels'] ) );
+		$this->assertEquals( 'Product', $config['labels']['singular_name'] );
+		$this->assertEquals( 'Products', $config['labels']['menu_name'] );
+	}
+}
