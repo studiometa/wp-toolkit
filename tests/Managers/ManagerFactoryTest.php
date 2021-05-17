68cdac8b56f709678ca328d9f113f124bb9b0d66
Lucas Simeon <lu.simeon@gmail.com>
2021-05-17 18:38:35 +0200

    Rewrite MagerFactoryTest with PHPUnit instead of Pest

--- tests/Managers/ManagerFactoryTest.php
+++ tests/Managers/ManagerFactoryTest.php
@@ -1,23 +1,33 @@
 <?php
 
-namespace Tests\Managers;
-
 use Studiometa\WPToolkit\Managers\ManagerInterface;
 use Studiometa\WPToolkit\Managers\ManagerFactory;
 
-it(
-	'should trigger the manager instances `run` method.',
-	function() {
-		/**
-		 * Dummy manager class.
-		 */
-		class Manager implements ManagerInterface {
-			public function run() {
-				test()->has_run = true;
-			}
-		}
-
-		ManagerFactory::init( array( new Manager() ) );
-		expect( $this->has_run )->toBe( true );
+/**
+ * Dummy manager class.
+ */
+class Manager implements ManagerInterface {
+	public function run() {
+		ManagerFactoryTest::$has_run = true;
 	}
-);
+}
+
+class ManagerFactoryTest extends WP_UnitTestCase {
+	public static $has_run = false;
+
+	/**
+	 * Test init method.
+	 * Should trigger the manager instances `run` method.
+	 *
+	 * @return void
+	 */
+	public function test_init() {
+		$this->assertFalse( self::$has_run );
+
+		ManagerFactory::init(
+			array( new Manager() )
+		);
+
+		$this->assertTrue( self::$has_run );
+	}
+}
