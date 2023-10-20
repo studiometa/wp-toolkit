9cb754e5aa9c19bd75bd340b5222f8dc1a7750c2
Titouan Mathis <titouan@studiometa.fr>
2023-10-20 11:30:45 +0200

    Add PHPMailer configuration tests

---
+++ tests/Managers/EmailManagerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use Studiometa\WPToolkit\Managers\EmailManager;
+
+/**
+ * EmailManagerTest test case.
+ */
+class EmailManagerTest extends WP_UnitTestCase {
+
+	/**
+	 * Test configure PHPMailer with SMTP server.
+	 *
+	 * @return void
+	 */
+	public function test_smtp_configuration() {
+		$_ENV['MAIL_MAILER']     = 'smtp';
+		$_ENV['MAIL_HOST']       = '127.0.0.1';
+		$_ENV['MAIL_PORT']       = '1025';
+		$_ENV['MAIL_USERNAME']   = 'test';
+		$_ENV['MAIL_PASSWORD']   = 'test';
+		$_ENV['MAIL_AUTH']       = 'true';
+		$_ENV['MAIL_ENCRYPTION'] = 'tls';
+
+		$manager = new EmailManager();
+		$manager->run();
+
+		// Trigger phpmailer configuration action.
+		global $phpmailer;
+		assert( $phpmailer instanceof PHPMailer\PHPMailer\PHPMailer );
+		do_action_ref_array( 'phpmailer_init', array( &$phpmailer ) );
+
+		// phpcs:disable WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+		$this->assertSame( $phpmailer->Mailer, 'smtp' );
+		$this->assertSame( $phpmailer->Host, '127.0.0.1' );
+		$this->assertSame( $phpmailer->Port, 1025 );
+		$this->assertSame( $phpmailer->Username, 'test' );
+		$this->assertSame( $phpmailer->Password, 'test' );
+		$this->assertSame( $phpmailer->SMTPAuth, true );
+		$this->assertSame( $phpmailer->SMTPSecure, 'tls' );
+		// phpcs:enable WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+	}
+}
