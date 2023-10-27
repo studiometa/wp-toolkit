04300c1c53bc6e9778ec222130ba5dfe8a2f5e2c
Titouan Mathis <titouan@studiometa.fr>
2023-10-27 15:44:03 +0200

    Add a RequestHelper

---
+++ src/Helpers/RequestHelper.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Request helper functions.
+ *
+ * @package    studiometa/wp-toolkit
+ * @author     Studio Meta <agence@studiometa.fr>
+ * @copyright  2021 Studio Meta
+ * @license    https://opensource.org/licenses/MIT
+ * @since      1.1.0
+ */
+
+namespace Studiometa\WPToolkit\Helpers;
+
+use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\InputBag;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+/**
+ * Request helper class.
+ */
+class RequestHelper {
+	/**
+	 * Request instance.
+	 *
+	 * @var Request|null
+	 */
+	private static $request = null;
+
+	/**
+	 * Get the Request instance.
+	 *
+	 * @return Request
+	 */
+	public static function get(): Request {
+		if ( is_null( static::$request ) ) {
+			static::$request = Request::createFromGlobals();
+		}
+
+		return static::$request;
+	}
+}
