f95323f34a87fa18a6ae0450af390651a00c0bd2
Titouan Mathis <titouan@studiometa.fr>
2024-03-20 16:57:18 +0100

    Add tests

--- tests/Helpers/EnvTest.php
+++ tests/Helpers/EnvTest.php
@@ -2,14 +2,14 @@
 
 namespace Studiometa\WPToolkitTest;
 
-use WP_UnitTestCase;
-use Studiometa\WPToolkit\Helpers\Env as EnvClass;
+use PHPUnit\Framework\TestCase;
+use Studiometa\WPToolkit\Helpers\Env;
 use function Studiometa\WPToolkit\env;
 
 /**
  * EnvTest test case.
  */
-class EnvTest extends WP_UnitTestCase
+class EnvTest extends TestCase
 {
 
     /**
@@ -20,6 +20,33 @@ class EnvTest extends WP_UnitTestCase
     public function test_type_of_request_function_helper()
     {
         $this->assertTrue(is_string(env('missing')));
-        $this->assertTrue(is_string(EnvClass::get('missing')));
+        $this->assertTrue(is_string(Env::get('missing')));
+    }
+
+    /**
+     * Test the `env_is_...` functions and methods.
+     *
+     * @return void
+     */
+    public function test_env_is_functions()
+    {
+        $mapping = [
+            'is_local'       => 'local',
+            'is_prod'        => 'production',
+            'is_prod'        => 'prod',
+            'is_preprod'     => 'preprod',
+            'is_development' => 'development',
+            'is_staging'     => 'staging',
+        ];
+
+        foreach ($mapping as $name => $value) {
+            $fn = sprintf('\Studiometa\WPToolkit\env_%s', $name);
+            $_ENV['APP_ENV'] = strtolower($value);
+            $this->assertTrue($fn());
+            $this->assertTrue(Env::$name());
+            $_ENV['APP_ENV'] = strtoupper($value);
+            $this->assertTrue($fn());
+            $this->assertTrue(Env::$name());
+        }
     }
 }
