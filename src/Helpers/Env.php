7e2e55f0162ac42978f10e70862c892cf47e9387
Titouan Mathis <titouan@studiometa.fr>
2024-03-20 16:34:37 +0100

    Add utility functions to test the current environment

--- src/Helpers/Env.php
+++ src/Helpers/Env.php
@@ -19,4 +19,67 @@ class Env
         // so we use `getenv` as a fallback to try and get the value.
         return $env[ $key ] ?? (string) getenv($key);
     }
+
+    /**
+     * Get the current APP_ENV configuration.
+     *
+     * @return string
+     */
+    private static function get_app_env(): string
+    {
+        return strtolower(self::get('APP_ENV'));
+    }
+
+    /**
+     * Test if the current environment is production.
+     * Both `production` and `prod` values are tested on the `APP_ENV` or
+     * `WP_ENV` environment variable or the `WP_ENV` constant.
+     *
+     * @return bool
+     */
+    public static function is_prod(): bool
+    {
+        $env = self::get_app_env();
+        return $env === 'production' || $env === 'prod';
+    }
+
+    /**
+     * Test if the current environment is preprod.
+     *
+     * @return bool
+     */
+    public static function is_preprod(): bool
+    {
+        return self::get_app_env() === 'preprod';
+    }
+
+    /**
+     * Test if the current environment is local.
+     *
+     * @return bool
+     */
+    public static function is_local(): bool
+    {
+        return self::get_app_env() === 'local';
+    }
+
+    /**
+     * Test if the current environment is staging.
+     *
+     * @return bool
+     */
+    public static function is_staging(): bool
+    {
+        return self::get_app_env() === 'staging';
+    }
+
+    /**
+     * Test if the current environment is development.
+     *
+     * @return bool
+     */
+    public static function is_development(): bool
+    {
+        return self::get_app_env() === 'development';
+    }
 }
